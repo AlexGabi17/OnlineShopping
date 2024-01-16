dfbb62ea2afcd964ddb210ce7bab72246dc06e60
AlexGabi17 <alexbuzescu@yahoo.com>
2024-01-16 13:06:12 +0200

    init project

---
+++ proiectFacultate/src/main/java/com/proiectfacultate/proiectfacultate/ProiectFacultateApplication.java
@@ -0,0 +1,13 @@
+package com.proiectfacultate.proiectfacultate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectFacultateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectFacultateApplication.class, args);
+    }
+
+}
---
+++ proiectFacultate/src/test/java/com/proiectfacultate/proiectfacultate/ProiectFacultateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiectfacultate.proiectfacultate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectFacultateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
